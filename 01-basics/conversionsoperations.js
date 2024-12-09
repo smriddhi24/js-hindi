8eecc4d1417c78359d48f07dd4e12bbac4148a86
smriddhi24 <124387966+smriddhi24@users.noreply.github.com>
2024-12-09 10:34:48 +0000

    conversions of datatypes

---
+++ 01-basics/conversionsoperations.js
@@ -0,0 +1,30 @@
+let score = "23"
+
+
+
+console.log(typeof score);
+
+let valueInNmuber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNmuber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+// 1 => true; 0 => false
+// "" => false 
+// "sam" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+// 
