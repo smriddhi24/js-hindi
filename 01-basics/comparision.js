3195f3b99cd4a646a71392cb6b6dd20169a4acdd
smriddhi24 <124387966+smriddhi24@users.noreply.github.com>
2024-12-25 13:05:29 +0000

    comparision

---
+++ 01-basics/comparision.js
@@ -0,0 +1,28 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// THESE TYPES OF CONVERSIONS COULD BE DIFFICULT 
+// IN MOST OF THE CASES WE AVOID THESES TYPES OF COMPARISIONS
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  ===
+
+console.log("2" === 2 );
+
+
+
+     
