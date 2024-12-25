f5cd6ccced24e8c1a87c31b0d18c78011719a329
smriddhi24 <124387966+smriddhi24@users.noreply.github.com>
2024-12-25 13:06:08 +0000

    conversion

--- 01-basics/conversionsoperations.js
+++ 01-basics/conversionsoperations.js
@@ -27,4 +27,41 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
 
-// 
+// ************************************Operations**********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// these are the basics operations
+
+consolelog(2+2);
+consolelog(2-2);
+consolelog(2*2);
+consolelog(2**3);
+consolelog(2/3);
+consolelog(2%2);
+
+let str1 = "sam"
+let str2 = " hello"
+let str3 = str1 + str2
+consolelog(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
